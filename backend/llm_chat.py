2ca03460130bd0e6b46eaf420359697d40d0d5c4
Wei Liu <liuweixa@redhat.com>
2024-08-07 20:09:17 +0800

    init
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- backend/llm_chat.py
+++ backend/llm_chat.py
@@ -1,13 +1,36 @@
 # coding: utf-8
 
-import os
-import cmd
+import os, cmd, getopt, sys
 
 from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
 
 from chat.engine import ChatEngine
+
+def get_args(argv):
+    model="llama3.1"
+    index_dir=os.path.join(os.getcwd(), "data", "example", "index")
+
+    try:
+        opts, _ = getopt.getopt(argv, "hm:i:",["model=","index-dir="])
+    except getopt.GetoptError:
+        print("llm_chat.py -m <ollama model> -i <data dir>")
+        sys.exit(1)
+    
+    if len(opts) == 0:
+        return model, index_dir
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("llm_chat.py -m <ollama model> -i <data dir>")
+            sys.exit()
+        elif opt in ("-m", "--model"):
+            model = arg
+        elif opt in ("-o", "--index-dir"):
+            index_dir = arg
+    return model, index_dir
+
 
 class LLMChat(cmd.Cmd):
     intro = 'Welcome to the chat shell.   Type help or ? to list commands.\n'
@@ -27,9 +50,11 @@ class LLMChat(cmd.Cmd):
         raise SystemExit
 
 if __name__ == '__main__':
-    index_dir=os.path.join(os.getcwd(), "backend", "data", "example", "index")
-    embed_model = OllamaEmbedding("llama3:8b")
-    llm = Ollama(model="llama3:8b", request_timeout=600.0)
+    model, index_dir=get_args(sys.argv[1:])
+    print("model: %s, index: %s" % (model, index_dir))
+
+    embed_model = OllamaEmbedding(model)
+    llm = Ollama(model=model, request_timeout=600.0)
     Settings.llm = llm
     Settings.embed_model = embed_model
     
--- backend/scripts/build_index.py
+++ backend/scripts/build_index.py
@@ -8,37 +8,39 @@ from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.readers.file import PDFReader
 #from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-script_name="build_index"
-
-def get_dirs(argv):
+def get_args(argv):
+    model="llama3.1"
     data_dir=os.path.join(os.getcwd(), "backend", "data", "example", "doc")
     index_dir=os.path.join(os.getcwd(), "backend", "data", "example", "index")
 
     try:
-        opts, _ = getopt.getopt(argv, "hi:o:",["data-dir=","index-dir="])
+        opts, _ = getopt.getopt(argv, "hm:i:o:",["model=","data-dir=","index-dir="])
     except getopt.GetoptError:
-        print("%s.py -i <data dir> -o <index dir>" % script_name)
+        print("build_index.py -m <ollama model> -i <data dir> -o <index dir>")
         sys.exit(1)
     
     if len(opts) == 0:
-        print("use example data")
-        return data_dir, index_dir
+        return model, data_dir, index_dir
 
     for opt, arg in opts:
         if opt == '-h':
-            print("%s.py -i <data dir> -o <index dir>" % script_name)
+            print("build_index.py -m <ollama model> -i <data dir> -o <index dir>")
             sys.exit()
+        elif opt in ("-m", "--model"):
+            model = arg
         elif opt in ("-i", "--data-dir"):
             data_dir = arg
         elif opt in ("-o", "--index-dir"):
             index_dir = arg
+    return model, data_dir, index_dir
 
 if __name__ == "__main__":
-    data_dir, index_dir=get_dirs(sys.argv[1:])
+    model, data_dir, index_dir=get_args(sys.argv[1:])
+    print("model: %s, data: %s, index: %s" % (model, data_dir, index_dir))
 
-    llm = Ollama(model="llama3:8b", request_timeout=120.0)
+    llm = Ollama(model=model, request_timeout=120.0)
     #embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    embed_model = OllamaEmbedding("llama3:8b")
+    embed_model = OllamaEmbedding(model)
 
     # Set model
     Settings.llm = llm
@@ -46,9 +48,11 @@ if __name__ == "__main__":
     
     # Load Data
     # TODO load data from different sources
+    print("loading data ...")
     docs = SimpleDirectoryReader(data_dir, file_extractor={".pdf": PDFReader()}).load_data()
     
     # Build index
+    print("build index ...")
     index = VectorStoreIndex.from_documents(docs)
     # TODO try to introduce a database
     index.storage_context.persist(persist_dir=index_dir)
